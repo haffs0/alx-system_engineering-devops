dcbe6775b37e5abda764e2323443e339ea0e89ec
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-12-23 14:11:35 +0000

    client config using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,10 +2,10 @@
 
 exec {
   command => "echo 'IdentityFile ~/.ssh/school' >> /etc/ssh/ssh_config",
-  path => '/usr/bin'
+  path => /usr/bin
 }
 
 exec {
   command => "echo 'PasswordAuthentication no' >> /etc/ssh/ssh_config",
-  path => '/usr/bin'
+  path => /usr/bin
 }
